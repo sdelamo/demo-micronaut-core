a6153de4723aa717adeb375cbe062e1816287ec2
sdelamo <sergio.delamo@softamo.com>
2018-07-12 15:13:49 +0200

    Add travis file

---
+++ travis-build.sh
@@ -0,0 +1,15 @@
+if [[ -n $TRAVIS_TAG ]]; then
+    version="$TRAVIS_TAG"
+    version=${version:1}
+
+    git clone https://${GH_TOKEN}@github.com/sdelamo/demo-static-website.git -b master static-website-master --single-branch > /dev/null
+    cd static-website-master
+    ./release $version
+
+    git commit -a -m "Updating static website for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" && {
+
+    cd ..
+
+    rm static-website-master
+fi
+
